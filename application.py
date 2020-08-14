2c413a7302aec38d428ee7be015f22c285a40c92
Matthew <msimanonis@gmail.com>
2020-08-14 11:30:15 -0500

    remove files

--- application.py
+++ application.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request
 from flask_debug import Debug
 import os
 
+application = app = Flask(__name__)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_TYPE"] = "filesystem"
-app.secret_key = 'asdfawfawerawerawerwerw453453454'
+app.secret_key = '08943u50930u9jfsajsdhfaseujfij345j9034u5asedfasdfasdf4'
 Debug(app)
 app.run(debug=True)
 
