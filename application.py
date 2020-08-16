87b30fb998f88a06349d212e0585c0d8fa36bc3a
Matthew <msimanonis@gmail.com>
2020-08-16 16:09:09 -0500

    aws

--- application.py
+++ application.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = '08943u50930u9jfsajsdhfaseujfij345j9034u5asedfasdfasdf4'
-Debug(app)
-app.run(debug=True)
 
 
 @app.route("/")
 def index():
     return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run()
